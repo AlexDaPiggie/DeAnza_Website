1399e81a483d95ecbc2b141ebe35f2f314650e77
nichelleeeeeee <149719072+nichelleeeeeee@users.noreply.github.com>
2026-07-24 10:43:27 -0700

    Update App.jsx

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,9 +1,8 @@
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div>
-      <h1>Transfer Planner</h1>
-      <p>Plan your transfer journey</p>
-    </div>
+    <Home />
   );
 }
 
